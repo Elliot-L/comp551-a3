22f3d2849a665721bdb4270b0065c2aa44d6ffc6
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-03-08 10:56:04 -0500

    updated for compatibility with torch's DataLoader and TensorDataset

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,8 @@
-import os, pickle
+import os, pickle, torch
 import pandas as pds 
+import numpy as np
 
-def load_training_labels( filepath ):
+def load_training_labels( filepath=os.path.join( os.getcwd(), 'train_labels.csv' ), as_tensor=False ):
     """
     Wrapper around pds.read_csv with the appropriate args.
 
@@ -13,14 +14,20 @@ def load_training_labels( filepath ):
 
         pandas DataFrame representation of the file (dtype = int64 ).
     """
-    return pds.read_csv( 
+    labels = pds.read_csv( 
         filepath, 
         sep=',',
         header=0,
         index_col=None
     )
 
-def load_training_data( filepath ):
+    if as_tensor:
+        return torch.tensor( np.array( labels.Category.values, dtype=np.float32 ) )
+    else:
+        return labels
+    
+
+def load_training_data( filepath=os.path.join( os.getcwd(), 'train_images.pkl' ), as_tensor=False ):
     """
     Wrapper around pickle.load with the appropriate args.
 
@@ -33,4 +40,10 @@ def load_training_data( filepath ):
         numpy 3D array representation of the file (dtype = float64 ).
     """
     with open( filepath, 'rb' ) as handle:
-        return pickle.load( handle )
+        data = pickle.load( handle )
+    
+    if as_tensor:
+        return torch.tensor( data )
+    
+    else:
+        return data
