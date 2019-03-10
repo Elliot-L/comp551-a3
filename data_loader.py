3dfb60bdb347fbbf941a150f4fc960b15423c1e7
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-03-09 23:59:43 -0500

    small update, lots of ongoing work except for biggest_bbox_extractor.py

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,4 @@
-import os, pickle, torch
+import os, pickle, torch, random
 import pandas as pds 
 import numpy as np
 
@@ -26,7 +26,6 @@ def load_training_labels( filepath=os.path.join( os.getcwd(), 'train_labels.csv'
     else:
         return labels
     
-
 def load_training_data( filepath=os.path.join( os.getcwd(), 'train_images.pkl' ), as_tensor=False ):
     """
     Wrapper around pickle.load with the appropriate args.
@@ -47,3 +46,31 @@ def load_training_data( filepath=os.path.join( os.getcwd(), 'train_images.pkl' )
     
     else:
         return data
+'''
+class FullTrainingDataset(torch.utils.data.Dataset):
+    def init(self, full_ds, offset, length):
+    self.full_ds = full_ds
+    self.offset = offset
+    self.length = length
+    assert len(full_ds)>=offset+length, Exception(“Parent Dataset not long enough”)
+    super(FullTrainingDataset, self).init()
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, i):
+        return self.full_ds[i+self.offset]
+
+    def trainTestSplit(dataset, val_share=TEST_RATIO):
+    val_offset = int(len(dataset)*(1-val_share))
+    return FullTrainingDataset(dataset, 0, val_offset), FullTrainingDataset(dataset, val_offset, len(dataset)-val_offset)
+
+    train_ds, val_ds = trainTestSplit(dset_train)
+
+    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE,
+    shuffle=True, num_workers=1,pin_memory=PIN_MEMORY)
+
+    val_loader = torch.utils.data.DataLoader(val_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=1,
+    pin_memory=PIN_MEMORY)
+
+'''
