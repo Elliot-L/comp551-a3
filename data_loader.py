3b2187acb27c718f8920eda05806e0074c42c442
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-03-14 18:39:40 -0400

    added load_testing_data function

--- data_loader.py
+++ data_loader.py
@@ -36,7 +36,7 @@ def load_training_data( filepath=os.path.join( os.getcwd(), 'train_images.pkl' )
 
     Returns:
 
-        numpy 3D array representation of the file (dtype = float64 ).
+        numpy 3D array/pytorch tensor representation of the file (dtype = float64 ).
     """
     with open( filepath, 'rb' ) as handle:
         data = pickle.load( handle )
@@ -46,3 +46,38 @@ def load_training_data( filepath=os.path.join( os.getcwd(), 'train_images.pkl' )
     
     else:
         return data
+
+def load_testing_data( filepath=os.path.join( os.getcwd(), 'test_images.pkl' ), as_tensor=False, pad_to_multiple_of=64 ):
+    """
+    Wrapper around pickle.load with the appropriate args and a padding option.
+
+    Arguments:
+
+        filepath: path to train_labels.csv 
+
+        as_tensor: boolean indicator of whether to return a pytorch tensor (True) or numpy array (False).
+
+        pad_to_multiple_of: int, enforced the returned dataset's first dimension to be a multiple of this parameter 
+                            (adds all-0 arrays until that is the case)
+
+    Returns:
+
+        numpy 3D array/pytorch tensor representation of the file (dtype = float64 )
+
+        number of all-0 arrays that were appended to the tail of the 3D numpy array/pytorch tensor.
+    """
+    with open( filepath, 'rb' ) as handle:
+        data = pickle.load( handle )
+
+    number_of_zero_64by64_arrays_to_add = pad_to_multiple_of - ( data.shape[0] % pad_to_multiple_of )
+    pad_arrays = np.zeros( ( number_of_zero_64by64_arrays_to_add, 64, 64), dtype=float )
+    dataset = np.concatenate( ( data, pad_arrays ), axis=0 )
+    
+    print( f"Note: dataset had {data.shape[0]} images, added {number_of_zero_64by64_arrays_to_add} zero arrays to get a dataset of shape {dataset.shape}" )
+
+    if as_tensor: 
+
+        return torch.tensor( dataset ), number_of_zero_64by64_arrays_to_add
+    
+    else:
+        return dataset, number_of_zero_64by64_arrays_to_add
