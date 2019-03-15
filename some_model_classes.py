8724405197b10d870bf6b0c10c217f821c4a004a
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-03-15 00:30:25 -0400

    began work on a deeper CNN

--- some_model_classes.py
+++ some_model_classes.py
@@ -191,3 +191,22 @@ class Other_MNIST_SANITY_CHECK_CNN(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
     
+class deeper_MNIST_CNN(nn.Module):
+    # deeper and bigger version of Other_MNIST_CNN
+    def __init__( self ):
+        super( deeper_MNIST_CNN, self ).__init__()
+        self.conv1 = nn.Conv2d( 1, 16, kernel_size=3, stride=2, padding=1 )
+        self.conv2 = nn.Conv2d( 16, 32, kernel_size=3, stride=2, padding=1 ) 
+        self.conv3 = nn.Conv2d( 32, 64, kernel_size=3, stride=2, padding=1 )
+        self.fc1 = nn.Linear( 2048, 1024 )
+        self.fc2 = nn.Linear( 1024, 512 )
+        self.fc3 = nn.Linear( 512, 10 )
+
+    def forward( self, x ):
+        x = F.relu( self.conv1( x ) )
+        x = F.max_pool2d( x, 2, 2 )
+        x = F.relu( self.conv2( x ) )
+        x = F.max_pool2d( x, 2, 2, )
+        x = F.relu( self.conv3 ( x ) )
+        x = F.max_pool2d( x, 2, 2 )
+        return 0
